086143f825962cfe0620fc958982926d722cc028
sharath38 <tsharath38@gmail.com>
2020-04-10 15:44:18 +0530

    Updates

---
+++ precache-manifest.d548b16ac75171cbbef68b04cbb5e8e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f39c660d57153bf89fbf4463e0a695d",
+    "url": "/Monster-robo/index.html"
+  },
+  {
+    "revision": "243f7f833ee485541ad5",
+    "url": "/Monster-robo/static/css/main.923f5f2f.chunk.css"
+  },
+  {
+    "revision": "a58e1ba730d0c1b0b6be",
+    "url": "/Monster-robo/static/js/2.9ace2ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-robo/static/js/2.9ace2ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243f7f833ee485541ad5",
+    "url": "/Monster-robo/static/js/main.b141f9cc.chunk.js"
+  },
+  {
+    "revision": "f17a55973b8afceb97d5",
+    "url": "/Monster-robo/static/js/runtime-main.6c3851ab.js"
+  }
+]);
